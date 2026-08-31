1a87b0257ab6871678bbbd69e81418ba33fdc575
Khalid0226 <ansarikhalid8054@gmail.com>
2026-08-31 11:36:43 +0530

    profile

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary'
+import streamifier from 'streamifier'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+export const uploadToCloudinary = (fileBuffer) =>{
+    return new Promise((resolve,reject)=>{
+        let stream = cloudinary.uploader.upload_stream(
+            {folder:"chatwave_profiles"},
+            (error,result)=>{
+                if(result){
+                    resolve(result)
+                }else{
+                    reject(error)
+                }
+            }
+        )
+        streamifier.createReadStream(fileBuffer).pipe(stream)
+    })
+}
