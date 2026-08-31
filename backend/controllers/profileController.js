2a35da403f3afe619c43607adee6be7c5af14ccf
Khalid0226 <ansarikhalid8054@gmail.com>
2026-08-31 11:44:19 +0530

    profile

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,30 +1,43 @@
 import userModel from "../models/userModel.js";
+import { uploadToCloudinary } from "../utils/cloudinary.js";
 
-export const updateProfile = async (req,res) => {
+export const updateProfile = async (req, res) => {
     try {
         const userId = req.user._id
-    const {name,phone,about} = req.body
+        const { name, phone, about } = req.body
 
-    const updateUser = await userModel.findByIdAndUpdate(
-        userId,
-        {name,phone,about},
-        {new:true,runValidators:true}
-    ).select('-password')
+        let profilePicUrl = undefined
 
-    if(!updateUser){
-        return res.status(404).json({
-            message:'user not found!!!'
+        if(req.file){
+            let cloudinaryResponse = await uploadToCloudinary(req.file.buffer);
+            profilePicUrl = cloudinaryResponse.secure_url
+        }
+
+        const updateData = {name,phone,about}
+        if(profilePicUrl){
+            updateData.avatar = profilePicUrl
+        }
+
+        const updateUser = await userModel.findByIdAndUpdate(
+            userId,
+            updateData,
+            { new: true, runValidators: true }
+        ).select('-password')
+
+        if (!updateUser) {
+            return res.status(404).json({
+                message: 'user not found!!!'
+            })
+        }
+
+        res.status(200).json({
+            message: 'profile updated successfully!!!',
+            updateUser
         })
-    }
-
-    res.status(200).json({
-        message:'profile updated successfully!!!',
-        updateUser
-    })
     } catch (error) {
         res.status(500).json({
-            message:'failed to update userProfile!!!',
-            error:error.message
-        })   
+            message: 'failed to update userProfile!!!',
+            error: error.message
+        })
     }
 }
